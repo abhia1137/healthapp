e9ab3051ffbbcf49b620f24c603f39c059e0edfc
Abhi <abhi.arora@geminisolutions.in>
2020-05-10 12:24:56 +0530

    Host name added

--- hello.js
+++ hello.js
@@ -1,21 +1,7 @@
-const http = require("http");
-// const hostname = '18.218.140.217';
-const express = require('express');
-var router = express.Router();
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+const express = require('express')
+const app = express()
+const port = 3000
 
-    //Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-router.get('/', function(req, res) {
-    res.send('good working')
-})
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(3000, () => {
-    console.log(`Server running at `);
-});
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
